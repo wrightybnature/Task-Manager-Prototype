ea19cd915b7962b249a5dda2c95e4bec0c834dce
Flick <felicitywright103@gmail.com>
2023-05-08 01:35:21 +0100

    Changed the css for the LoginSignUp page because the log in section looked too similar to the sign up section.

--- src/LoginSignUpPage.js
+++ src/LoginSignUpPage.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './LoginSignUpPage.css';
+import './SignUpPage.css';
+import {
+  faFacebookF,
+  faTwitter,
+  faGoogle,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function LoginSignUpPage() {
   const handleLogin = (e) => {
@@ -21,12 +29,26 @@ function LoginSignUpPage() {
   return (
     <div className="login-signup-container">
       <div className="login-container">
-        <h1>Log in to Your Account</h1>
+        <div className="g-darkest">
+          <h1>Task Manager</h1>
+        </div>
+        <h1 className="g-dark">Log in to Your Account</h1>
         <div className="social-buttons">
-          {/* Add the social media login buttons here */}
+          <button className="social-btn facebook-btn">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </button>
+        <button className="social-btn google-btn">
+          <FontAwesomeIcon icon={faGoogle} />
+        </button>
+        <button className="social-btn twitter-btn">
+          <FontAwesomeIcon icon={faTwitter} />
+        </button>
+        <button className="social-btn linkedin-btn">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </button>
         </div>
         <div className="divider">
-          <span>or</span>
+          <span className="g-dark">or</span>
         </div>
         <form onSubmit={handleLogin}>
           <input type="email" name="email" placeholder="Email" required />
@@ -35,9 +57,9 @@ function LoginSignUpPage() {
         </form>
       </div>
       <div className="signup-container">
-        <h1>Sign Up</h1>
-        <p>Keep track of all your tasks by making an account now!</p>
-        <button className="submit" onClick={() => window.location.href = '/signup'}>Sign up</button>
+        <h1 className="blue">Sign Up</h1>
+        <p className="blue">Keep track of all your tasks by making an account now!</p>
+        <button className="submit-signup" onClick={() => window.location.href = '/signup'}>Sign up</button>
         
       </div>
     </div>
