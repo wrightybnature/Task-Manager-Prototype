321711477c4ca3f769e64cddfc66b7605ad4a110
Flick <felicitywright103@gmail.com>
2023-05-12 13:53:37 +0100

    Fixing the Settings page to include more accessibility options. Refactored the css page for the settings so that it is similar to the rest of the website.

--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun, faTextHeight, faLanguage, faBell } from '@fortawesome/free-solid-svg-icons';
 import './SettingsPage.css';
 
 function SettingsPage() {
+  // State for user's name and email
   const [userName, setUserName] = useState('Felicity');
   const [userEmail, setUserEmail] = useState('felicity@example.com');
 
+  // Handlers for name and email change
   const handleNameChange = (e) => {
     setUserName(e.target.value);
   };
@@ -15,33 +19,92 @@ function SettingsPage() {
   };
 
   return (
-    <div>
+    <div className="settings-container"> {/* Container for centering the content */}
       <h1>Settings</h1>
       <h2>Account</h2>
+
+      {/* Name input field */}
       <label htmlFor="name">Name:</label>
       <input
         type="text"
         id="name"
         value={userName}
         onChange={handleNameChange}
-        className="settings-input"
+        className="settings-input name-input"
       />
+
+      {/* Email input field */}
       <label htmlFor="email">Email:</label>
       <input
         type="email"
         id="email"
         value={userEmail}
         onChange={handleEmailChange}
-        className="settings-input"
+        className="settings-input email-input"
       />
-      <button className="settings-button">Reset Password</button>
-      <br />
-      <br />
-      <h2>Other</h2>
-      <button className="settings-button">Support</button>
-      <br />
-      <br />
-      <Link to="/calendar/dayGridMonth">Back</Link>
+
+      {/* Reset Password button */}
+      <button className="settings-button reset-password-button">Reset Password</button>
+
+      <h2>Accessibility</h2>
+
+      {/* Dark Mode binary slider */}
+      <div className="settings-label">
+        <FontAwesomeIcon icon={faMoon} className="settings-icon" />
+        <span>Dark Mode</span>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      {/* Brightness slider */}
+      <div className="settings-label">
+        <FontAwesomeIcon icon={faSun} className="settings-icon" />
+        <span>Brightness</span>
+        <input type="range" min="1" max="100" className="slider" />
+      </div>
+
+      {/* Text Size dropdown */}
+      <div className="settings-label">
+        <FontAwesomeIcon icon={faTextHeight} className="settings-icon" />
+        <span>Text Size</span>
+        <select className="dropdown">
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+        </select>
+      </div>
+
+      {/* Language dropdown */}
+      <div className="settings-label">
+        <FontAwesomeIcon icon={faLanguage} className="settings-icon" />
+        <span>Language</span>
+        <select className="dropdown">
+          <option>English</option>
+          <option>Spanish</option>
+          <option>French</option>
+        </select>
+      </div>
+
+      {/* Notifications binary slider */}
+      <div className="settings-label">
+        <FontAwesomeIcon icon={faBell} className="settings-icon" />
+        <span>Notifications</span>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      <h3>Other</h3>
+
+      {/* Support button */}
+      <button className="settings-button support-button">Support</button>
+
+      {/* Back button */}
+      <Link to="/calendar/dayGridMonth" className="settings-button back-button">Back
+      </Link>
     </div>
   );
 }
